33f395e86b4561ba6f6b9764ba942730f9cc87c7
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2019-12-02 16:37:24 +0100

    sdf

--- CarManagement/Program.cs
+++ CarManagement/Program.cs
@@ -7,6 +7,7 @@ namespace CarManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Car");
+            Console.WriteLine("Is");
             Console.ReadKey();
         }
     }
